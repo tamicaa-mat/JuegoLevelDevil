e5d095a89b4be09ecc8fd4a030053def32268ca7
damian19931993 <124803816+damian19931993@users.noreply.github.com>
2024-05-22 22:17:31 -0300

    Update Monedas.cpp

--- Monedas.cpp
+++ Monedas.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include <SFML/Graphics.hpp>
 
 #include"Monedas.h"
@@ -14,26 +14,8 @@ Moneda::Moneda()
 
 sf::CircleShape& Moneda::getDraw()
 {
-    // Código de la función getDraw()
+    // CÃ³digo de la funciÃ³n getDraw()
     return _shapeMoneda;
 }
-=======
-#include <SFML/Graphics.hpp>
 
-#include"Monedas.h"
 
-Moneda::Moneda()
-{
-    _shapeMoneda.setFillColor(sf::Color::Yellow);
-    _shapeMoneda.setRadius(10);
-    _shapeMoneda.setPosition(300,200);
-    _visible=true;
-
-}
-
-sf::CircleShape& Moneda::getDraw()
-{
-    // Código de la función getDraw()
-    return _shapeMoneda;
-}
->>>>>>> 6f61caf099bd79490bf4755cf2e83f910632a5e9
