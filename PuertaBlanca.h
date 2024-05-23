925f407605b98506ca7b40baa4eef78e7f85f23d
damian19931993 <124803816+damian19931993@users.noreply.github.com>
2024-05-22 22:23:02 -0300

    Update PuertaBlanca.h

--- PuertaBlanca.h
+++ PuertaBlanca.h
@@ -15,47 +15,6 @@ public:
  sf::RectangleShape& getDraw();
 
 };
-
-
-
-
-
-
-
-
-
-
-
-
 #endif // PUERTABLANCA_H_INCLUDED
 
-#ifndef PUERTABLANCA_H_INCLUDED
-#define PUERTABLANCA_H_INCLUDED
-#include <SFML/Graphics.hpp>
-
-class PuertaBlanca
-{
-private:
-    sf::RectangleShape _puertaRec;
-    sf::Vector2f _posicionPuertaBlanca;
-public:
-    PuertaBlanca(float ancho, float altura);
-
-    // Método para dibujar la puerta
- sf::RectangleShape& getDraw();
-
-};
-
-
-
-
-
-
-
-
-
-
-
-
-#endif // PUERTABLANCA_H_INCLUDED
 
